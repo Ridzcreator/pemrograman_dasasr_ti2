448bd861a4d501a82696855ded41236df0dedf0a
NRidz <35356539+Ridzcreator@users.noreply.github.com>
2021-11-19 08:24:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pemrograman Dasar Ti2 </title>
+</head>
+<body>
+	<h1>PEMROGRAMAN DASAR</h1>
+</body>
+</html>
+
---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Form </title>
+<style>
+	body{
+	text-align:center;
+	}
+
+	h1 {
+	color:blue;
+	}
+	button {
+	background-color: #008CBA;
+	}
+</style>
+</head>
+<body>
+<h1>Login</h1>
+<form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+</form> 
+  <button class="button">Submit</button>
+
+</body>
+</html>
+
